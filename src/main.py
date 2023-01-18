d5ee462eb9e4ba951e09f6ff3e30c30e7da9e151
carlo-astro <carlo@ufrj.br>
2023-01-17 23:01:52 -0300

    First scenario attempt

---
+++ src/main.py
@@ -0,0 +1,82 @@
+class Cube:
+    def __init__(self):
+        self.is_held = False
+        self.name = "a cube"
+
+    def use(self):
+        self.is_held = True
+
+    def show(self):
+        print("you hold "+self.name)
+
+
+class Door:
+    def __init__(self):
+        self.is_held = False
+        self.name = "the door"
+
+    def use(self):
+        self.is_held = True
+
+    def show(self):
+        print("you hold "+self.name)
+
+
+class Button:
+    def __init__(self):
+        self.is_held = False
+        self.name = "the button"
+
+    def use(self):
+        self.is_held = True
+
+    def show(self):
+        print("you hold "+self.name)
+
+
+class Wall:
+    def __init__(self):
+        self.is_held = False
+        self.name = "the wall"
+
+    def use(self):
+        self.is_held = True
+
+    def show(self):
+        print("you hold "+self.name)
+        print("you try to pick up the wall but it is too heavy")
+
+
+class Joao:
+    def __init__(self):
+        self.is_held = False
+
+    def use(self, thing):
+        self.is_held = thing
+        thing.show()
+
+
+class Scenario:
+    def __init__(self):
+        self.hero = Joao()
+        self.view = Wall()
+        self.exit = Door()
+        self.thing = Cube()
+        self.action = Button()
+
+    def use(self):
+        thing = input("Use: 1-door, 2-button, 3-cube, 4-wall")
+        thing = int(thing) - 1
+        whatever = [self.exit, self.action, self.thing, self.view]
+        self.hero.use(whatever[thing])
+
+
+def main():
+    print("Welcome to Porteira!")
+    print("you can see a white wall, a button, a cube and a door")
+    scenario = Scenario()
+    scenario.use()
+
+
+if __name__ == '__main__':
+    main()
